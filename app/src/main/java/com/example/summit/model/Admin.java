0e26b01ff7ddb1881bb7d48d6de90c8e54743acd
aniketh1409 <anikeths@ualberta.ca>
2025-10-29 19:42:20 -0600

    abstract method signature for admin

--- app/src/main/java/com/example/summit/model/Admin.java
+++ app/src/main/java/com/example/summit/model/Admin.java
@@ -1,4 +1,12 @@
 package com.example.summit.model;
 
+
+//complete implementation
 public class Admin extends User {
+    public Admin() {}
+
+    @Override
+    public String getRole() {
+        return "";
+    }
 }
