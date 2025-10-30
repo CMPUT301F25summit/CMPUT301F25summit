aebfeb7feff7941e54bff75c079320653dc8e45d
aniketh1409 <anikeths@ualberta.ca>
2025-10-29 19:18:22 -0600

    basic entrant skeleton and abstract user

--- app/src/main/java/com/example/summit/model/User.java
+++ app/src/main/java/com/example/summit/model/User.java
@@ -1,5 +1,30 @@
 package com.example.summit.model;
 
-public abstract class User {
-    
+import java.io.Serializable;
+
+public abstract class User implements Serializable {
+    protected String id;
+    protected String name;
+    protected String email;
+    protected String deviceId;
+
+    public User(String id, String name, String email, String deviceId) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.deviceId = deviceId;
+    }
+
+    public User() {} // overloading for firebase implementation later ;)
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getDeviceId() { return deviceId; }
+
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+
+    public abstract String getRole(); //for role specific behavior
+
 }
