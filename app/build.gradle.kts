7c2eb62958ade094683daa99f201fe85ee014c1a
Kai Tan <kait069111@gmail.com>
2025-11-02 18:22:37 -0700

    Implement random sample in lottery system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,12 +40,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
--- app/src/main/java/com/example/summit/model/LotterySystem.java
+++ app/src/main/java/com/example/summit/model/LotterySystem.java
@@ -1,22 +1,30 @@
 package com.example.summit.model;
 
+import static java.util.Collections.shuffle;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LotterySystem {
     /*TODO: implement lottery logic - random selection
-    - draw entrants randomly from waitinglist
     - update entrant invitation status (they get accept/decline option)
     */
 
-    public LotterySystem() {}
-
-    public List<Entrant> sampleEntrants(WaitingList list, int spots) {
-        /*todo: 1. implement random selection
-        I've just put in a stub for now, delete/comment when actual implementation is done*/
-        return list.getEntrants().subList(0, Math.min(spots, list.getTotalEntrants()));
+    public LotterySystem() {
     }
 
+    /***
+     * Randomly samples entrants from a waiting list
+     *
+     * This works under the assumption that all entrants in the waiting list can be chosen
+     * for the event (Users invited already shouldn't be in the waitinglist)
+     *
+     * returns - List of randomly sampled entrants
+     */
+    public List<Entrant> sampleEntrants(WaitingList list, int spots) {
 
-
+        shuffle(list.getEntrants());
+        return list.getEntrants().subList(0, Math.min(spots, list.getTotalEntrants()));
+    }
 }
