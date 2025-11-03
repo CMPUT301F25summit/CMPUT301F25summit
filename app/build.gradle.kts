9f3f7c75b3a7dfb0669efb136a0a23b130776c3f
rabewarmor <rabewar@ualberta.ca>
2025-11-02 22:06:51 -0700

    fixed missing UI changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
+
 }
 
 android {
@@ -40,12 +41,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.firebase:firebase-firestore-ktx")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
